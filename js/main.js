a7ecc74b4a39c4e396706cc64b77d3499cfb401d
Quinton Jason <quinton.jason@gmail.com>
2024-10-03 10:57:53 -0500

    added javascript to second form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
 // JS scripts placed here
+
+
+// function handleClick(ev) {
+//     // Prevent the default action of the event
+//     ev.preventDefault();
+
+//     // declare variables for use
+//     const body = document.querySelector('body');
+
+//     // Get the value of the input field
+//     const nameInput = document.getElementById('name');
+//     const nameValue = nameInput.value;
+//     console.log('nameValue:', nameValue);
+//     alert('You clicked me!, ' + nameValue);
+
+//     // Perform action: add class to body element
+//     body.classList.add('form-submitted');
+// }
+
+const button = document.getElementById('submit');
+
+button.addEventListener("click", (ev) => {
+    // Prevent the default action of the event
+    ev.preventDefault();
+
+    // declare variables for use
+    const body = document.querySelector('body');
+    const nameOutput = document.getElementById('form-name');
+    console.log(nameOutput);
+
+
+    // Get the value of the input field
+    const nameInput = document.getElementById('name');
+    const nameValue = nameInput.value;
+    console.log('nameInput:', nameInput);
+    console.log('nameValue:', nameValue);
+    nameOutput.innerHTML = nameValue;
+    // alert('You clicked me!, ' + nameValue);
+
+    // Perform action: add class to body element
+    body.classList.add('form-submitted');
+
+    window.scrollTo(0, 0);
+});
